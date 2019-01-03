45c1d5bd11a5e017a5d0a96fa7f1a38e97a7da4f
SaraAtia <sara.atia111@gmail.com>
2019-01-03 11:55:16 +0200

    add classes

---
+++ SearchSolver.cpp
@@ -0,0 +1,14 @@
+//
+// Created by sara on 03/01/19.
+//
+
+#include "SearchSolver.h"
+template<class P, class S>
+SearchSolver<P,S>::SearchSolver(Searcher<S>* searcher){
+    this->searcher = searcher;
+}
+
+template<class P, class S>
+S* SearchSolver<P,S>::solve(Searchable<S> *p){
+    return this->searcher->search(p);
+}
---
+++ SearchSolver.h
@@ -0,0 +1,20 @@
+//
+// Created by sara on 03/01/19.
+//
+
+#ifndef PROJ2_SEARCHSOLVER_H
+#define PROJ2_SEARCHSOLVER_H
+
+#include "Solver.h"
+#include "Searcher.h"
+
+template <class P, class S>
+class SearchSolver : Solver<P, S>{
+    Searcher<S>* searcher;
+public:
+    SearchSolver(Searcher<S>* searcher);
+    S *solve(Searchable<S> *p) override;
+};
+
+
+#endif //PROJ2_SEARCHSOLVER_H
