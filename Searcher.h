9d99f48fe5b53c5397c8d491d7d7365116c4c449
paz1 <paz11paz@gmail.com>
2019-01-06 17:55:32 +0200

    change searcher plus bfs

--- Searcher.h
+++ Searcher.h
@@ -7,11 +7,13 @@
 
 #include "Solver.h"
 #include "Searchable.h"
-
+#include <vector>
 template <class S>
 class Searcher{
-    S *search(Searchable<S> *s);
+public:
+    virtual vector<State<S>*> search(Searchable<S> *s)=0;
     //int getNumberOfNodesEvaluated();
 
 };
 #endif //PROJ2_SEARCHER_H
+
